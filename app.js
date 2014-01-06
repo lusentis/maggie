7b38654a201b2e2b08801c8e9afb304064803b7c
Simone Lusenti <lusenti.s@gmail.com>
2014-01-06 19:30:05 +0100

    ranges

--- app.js
+++ app.js
@@ -79,18 +79,32 @@ ircc.on('message', function (nick, to, text) {
     
     _getMeta({ title: title, originalTitle: title, subtitles: 'no' }, function (err, meta) {
       if (err) {
+        ircc.say(nick, '[ x ]');
         logger.error('Error getting meta', err);
         return;
       }
       
-      if (meta === null) {
+      if (meta === null || (Array.isArray(meta) && meta.length < 1)) {
+        ircc.say(nick, '[   ]');
         logger.warn('Skipping torrent file because meta is undefined', title);
         return;
       }
       
       logger.debug('Got meta', title, meta);
-      _addTx(meta.magnet, function _addCallback(argument) {
-        ircc.say('(.)');
+      
+      if (!Array.isArray(meta)) {
+        meta = [meta];
+      }
+      
+      meta.forEach(function (meta_item) {
+        if (parseInt(meta_item.peers, 10) + parseInt(meta_item.seeds, 10) < 100) {
+          logger.warn('Episode', meta_item.episode, 'has too few peers.');
+          ircc.say(nick, '[ x ] ' + meta_item.season + 'x' + meta_item.episode + ': slow!');
+        }
+        
+        _addTx(meta_item.magnet, function _addCallback(argument) {
+          ircc.say(nick, '[ o ] ' + meta_item.season + 'x' + meta_item.episode + ': ' + meta_item.seeds + '/' + meta_item.peers + ' (' + meta_item.size + ')');
+        });
       });
     });
     
@@ -163,47 +177,100 @@ function _getMeta(ret, next) {
   }
 
   ret.title = parts[1];
-  ret.season = parseInt(parts[2], 10);
-  ret.episode = parseInt(parts[3], 10);
-  ret.episodeSpecial = parts[4] || '';
-  ret.quality = parts[5] || '';
-  ret.extra = parts[6] || '';
-  ret.torrentSearch = [
-    ret.title, ' ',
-    's' + ((ret.season < 10) ? '0' + ret.season : ret.season),
-    'e' + ((ret.episode < 10) ? '0' + ret.episode : ret.episode) + ret.episodeSpecial,
-    ' ', ret.quality, ret.extra
-  ].join('').trim();
+  ret.season = parseInt(parts[3], 10);
+  ret.episode = parseInt(parts[4], 10);
+  ret.episodeTo = parts[5] && parseInt(parts[5].substring(1), 10);
+  ret.isEpisodeRange = !!ret.episodeTo;
+  ret.episodeSpecial = parts[6] || '';
+  ret.fullSeason = parts[7] || null;
+  ret.isFullSeason = !!parts[8];
+  ret.quality = parts[9] || '';
+  ret.extra = parts[10] || '';
+  
+  // - Torrent search query
+  ret.torrentSearch = _buildSearchQuery(ret);
+  
+  // - handing episodes range
+  if (ret.isEpisodeRange) {
+    async.map(Number.range(ret.episode, ret.episodeTo).every(), function (episodeNumber, innerNext) {
+      var current = Object.clone(ret);
+      current.episode = episodeNumber;
+      current.torrentSearch = _buildSearchQuery(current);
+      
+      _searchAdd(current.torrentSearch.escapeURL(true), function (_, first_result) {
+        if (!first_result) {
+          logger.error('Cannot get info_hash from web service (isEpisodeRange===true):', current.torrentSearch, first_result);
+          innerNext(null, null);
+          return;
+        }
+        
+        current.infoHash = first_result[1];
+        current.size = first_result[3];
+        current.seeds = first_result[4];
+        current.peers = first_result[5];
+        current.magnet = 'magnet:?xt=urn:btih:' + current.infoHash + '&' + TRACKERS;
+        innerNext(null, current);
+      });
+      
+    }, function (_, episodes) {
+      next(null, episodes.compact());
+    });
+    
+  } else {
+    
+    logger.log('Req', TORRENTZ_URL.assign({ q: ret.torrentSearch.escapeURL(true) }));
+    logger.debug('Parsed meta', ret);
 
-  logger.log('Req', TORRENTZ_URL.assign({ q: ret.torrentSearch.escapeURL(true) }));
+    _searchAdd(ret.torrentSearch.escapeURL(true), function (_, first_result) {
+      if (!first_result) {
+        logger.error('Cannot get info_hash from web service (isEpisodeRange===false):', ret.torrentSearch, first_result);
+        next(null, null);
+        return;
+      }
+      
+      ret.infoHash = first_result[1];
+      ret.size = first_result[3];
+      ret.seeds = first_result[4];
+      ret.peers = first_result[5];
+      ret.magnet = 'magnet:?xt=urn:btih:' + ret.infoHash + '&' + TRACKERS;
+      next(null, ret);
+    });
+  }
+}
 
+
+function _buildSearchQuery(ret) {
+  var query = [ret.title, ' '];
+  if (ret.isFullSeason) {
+    query.push(ret.fullSeason);
+  } else {
+    query.push([
+      's' + ((ret.season < 10) ? '0' + ret.season : ret.season),
+      'e' + ((ret.episode < 10) ? '0' + ret.episode : ret.episode) + ret.episodeSpecial,
+      ' ', ret.quality, ret.extra
+    ]);
+  }
+  query = query.flatten().join('').trim();
+  return query;
+}
+
+
+function _searchAdd(query, cb) {
   request({
-    url: TORRENTZ_URL.assign({ q: ret.torrentSearch.escapeURL(true) })
+    url: TORRENTZ_URL.assign({ q: query })
   , strictSSL: true
   , headers: { 'Accept': '*/*', 'User-Agent': MACHINE.user_agent }
   }, function (err, _, body) {
     if (err) {
       logger.error('Torrent search error');
-      next(null, null);
+      cb(null, null);
       return;
     }
     
     var first_result = new RegExp(MACHINE.torrentz_re, 'im').exec(body);
-    if (!first_result) {
-      logger.error('Cannot get info_hash from web service.');
-      next(null, null);
-      return;
-    }
-    
-    ret.infoHash = first_result[1];
-    ret.size = first_result[3];
-    ret.seeds = first_result[4];
-    ret.peers = first_result[5];
-    ret.magnet = 'magnet:?xt=urn:btih:' + ret.infoHash + '&' + TRACKERS;
-    next(null, ret);
+    cb(null, first_result);
   });
 }
-
 
 
 // Add torrent to transmission
